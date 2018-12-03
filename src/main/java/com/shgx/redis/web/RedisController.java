fa7ab3dfeba3cb94e7ec78f664c30c98f7006c8c
guangxush <guangxush@163.com>
2018-12-03 23:00:21 +0800

    redis

--- src/main/java/com/shgx/redis/web/RedisController.java
+++ src/main/java/com/shgx/redis/web/RedisController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.shgx.redis.model.User;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
